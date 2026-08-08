a66cf90ee5147a3fd2fbc85fc82a8cc4761e70ec
kazimoujaddeisani <kazimoujaddei@gmail.com>
2026-08-09 03:53:25 +0600

    Update PWA cache for Firebase team system

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = "study-tracker-v1";
+const CACHE_NAME = "study-tracker-v2";
 
 const urlsToCache = [
   "./",
   "./index.html",
   "./style.css",
   "./script.js",
+  "./firebase-team.js",
   "./subject.js",
   "./manifest.json",
   "./data/bangla.js",
@@ -13,20 +14,24 @@ const urlsToCache = [
   "./data/gk.js"
 ];
 
-// Install
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
-// Fetch
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
